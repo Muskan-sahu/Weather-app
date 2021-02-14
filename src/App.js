aa4844fe5f0b9574a96d80c58ac3b5897e19237b
Muskan-sahu <65756003+Muskan-sahu@users.noreply.github.com>
2021-02-14 14:09:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@
 
 
  import React, { useState } from 'react';
+ import {
+  BrowserView,
+  MobileView
+} from "react-device-detect";
  const api = {
    key:"af9950e3e21db56623c5222e58b3a129",
    base: "https://api.openweathermap.org/data/2.5/"
@@ -37,6 +41,8 @@
    }
  
    return (
+    <div>
+    <MobileView>
      <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
        <main>
          <div className="search-box">
@@ -65,6 +71,38 @@
          ) : ('')}
        </main>
      </div>
+</MobileView>
+<BrowserView>
+        <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app2') : 'app2'}>
+       <main>
+         <div className="search-box">
+           <input 
+             type="text"
+             className="search-bar"
+             placeholder="Search..."
+             onChange={e => setQuery(e.target.value)}
+             value={query}
+             onKeyPress={search}
+           />
+         </div>
+         {(typeof weather.main != "undefined") ? (
+         <div>
+           <div className="location-box">
+             <div className="location">{weather.name}, {weather.sys.country}</div>
+             <div className="date">{dateBuilder(new Date())}</div>
+           </div>
+           <div className="weather-box">
+             <div className="temp">
+               {Math.round(weather.main.temp)}°c
+             </div>
+             <div className="weather">{weather.weather[0].main}</div>
+           </div>
+         </div>
+         ) : ('')}
+       </main>
+     </div>
+        </BrowserView>
+</div>
    );
  }
  
